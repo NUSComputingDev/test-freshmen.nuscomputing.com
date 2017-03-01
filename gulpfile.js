3b222ebc9876effa3ede89b2789b786a6b1c5b02
Yi Min <imleeyimin@gmail.com>
2017-03-01 21:41:40 +0800

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -31,58 +31,58 @@ let jadeUtils = {
   slugify: (str) => {
     return str.toLowerCase()
       .replace(/[^\w\d]+/g, '-');
-  }, 
-  
+  },
+
   url: (url) => {
     if (!env.urlRewrite)
       url += '.html';
-    
-    return env.locals.baseUrl + url; 
-  }, 
-  
-  moment, 
-  
+
+    return env.locals.baseUrl + url;
+  },
+
+  moment,
+
   faces: (photos, partitions) => {
     const dir = path.join(config.paths.static, photos);
-    partitions = partitions || []; 
-    
+    partitions = partitions || [];
+
     const people = fs.readdirSync(dir).map((file) => {
-      // Each file name should be of the form: 
-      //      00 Soh Wei Hao | Project Director.jpg 
-      // where the first two digits are the optional ordering, and the 
-      // rest of the filename is the name followed by the person's job, 
-      // delimited by the pipe character, since that is not a character that 
-      // occurs frequently in names or job titles. 
-      const photo = file.split('.')[0]; 
-      const name = photo.split(/\s*\|\s*/, 1)[0].trim();
-      
+      // Each file name should be of the form:
+      //      00 Soh Wei Hao | Project Director.jpg
+      // where the first two digits are the optional ordering, and the
+      // rest of the filename is the name followed by the person's job,
+      // delimited by the pipe character, since that is not a character that
+      // occurs frequently in names or job titles.
+      const photo = file.split('.')[0];
+      const name = photo.split(/\s*-\s*/, 1)[0].trim();
+
       return {
         name: name.replace(/\d+\s*/, ''),
-        file: env.locals.baseUrl + photos + '/' + file, 
-        job: _.trim(photo.substring(name.length), ' |'), 
+        file: env.locals.baseUrl + photos + '/' + file,
+        job: _.trim(photo.substring(name.length), ' -'),
       };
     });
-    
-    let groups = [people]; 
-    
+
+    let groups = [people];
+
     partitions.forEach((search, i) => {
       let splitted = _.partition(groups[i], (person) => {
-        return person.job.includes(search); 
-      }); 
-      
+        return person.job.includes(search);
+      });
+
       groups[i] = splitted[0];
       groups[i+1] = splitted[1];
     });
-    
+
     groups = groups.map((group) => {
       return _.chain(group)
         .groupBy('job')
         .sortBy('length')
         .flatten()
-        .value(); 
-    }); 
-    
-    return partitions.length ? groups : groups[0]; 
+        .value();
+    });
+
+    return partitions.length ? groups : groups[0];
   },
 };
 
@@ -94,7 +94,7 @@ function copyAssets(dest) {
   gulp.src(config.paths.etc + 'favicon.ico')
     .pipe(newer(dest))
     .pipe(gulp.dest(dest));
-  
+
   return gulp.src(staticPaths(), { base: config.paths.static })
     .pipe(newer(dest))
     .pipe(gulp.dest(dest));
@@ -102,7 +102,7 @@ function copyAssets(dest) {
 
 function buildSass(out, minify, reloadBS, useSourcemap) {
   const outputStyle = minify ? 'compressed' : 'nested';
-  
+
   return gulp.src(config.paths.styles + 'app.scss')
     .pipe(gulpif(useSourcemap, sourcemaps.init()))
     .pipe(sass({ outputStyle }).on('error', sass.logError))
@@ -133,9 +133,9 @@ gulp.task('serve', ['sass', 'templates'], () => {
       baseDir: env.output
     }
   });
-  
+
   gulp.watch(staticPaths(), ['assets-watch']);
-  
+
   gulp.watch(config.paths.styles + '**/*.scss', ['sass']);
   gulp.watch(config.paths.templates + '**/*.jade', ['templates-watch']);
 });
@@ -143,14 +143,14 @@ gulp.task('serve', ['sass', 'templates'], () => {
 gulp.task('build', () => {
   if (!_.has(config.environments, argv.env))
     throw new Error(`Environment '${argv.env}' not found. Please check your config.json`);
-  
+
   env = _.defaults(config.environments[argv.env], defaults);
 
   copyAssets(env.output);
   buildTemplates(env.locals, env.output);
   buildSass(env.output, env.minify, env.reload, env.sourcemaps);
-  
-  // Copy .htaccess file if necessary 
+
+  // Copy .htaccess file if necessary
   if (env.urlRewrite) {
     gulp.src(config.paths.etc + '.htaccess')
       .pipe(newer(env.output))
